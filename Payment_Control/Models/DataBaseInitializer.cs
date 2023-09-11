fb8cbe877e3636b0f269a086fa8189da4df86c81
Alex <sakalinski95@gmail.com>
2023-09-11 15:38:19 +0300

    Delete unused namespaces

--- Payment_Control/Models/DataBaseInitializer.cs
+++ Payment_Control/Models/DataBaseInitializer.cs
@@ -1,10 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Data.Entity;
-using System.Linq;
-using System.Security.RightsManagement;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Payment_Control.Models
 {
--- Payment_Control/Models/Income.cs
+++ Payment_Control/Models/Income.cs
@@ -1,12 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
-using System.Windows.Markup;
 
 namespace Payment_Control.Models
 {
